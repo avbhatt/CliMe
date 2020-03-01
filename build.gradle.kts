c2ae71c79cfe96b4517b8e107e2f64085c2f27ab
Adit Bhatt <avbhatt@umich.edu>
2020-03-01 15:38:31 -0500

    refactor: migrate to kotlin gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    id("maven")
+}
+
+group = "com.github.avbhatt"
+version = "LOCAL-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    uri("https://jitpack.io")
+}
+
+dependencies {
+    compile(group = "org.reflections", name = "reflections", version = "0.9.11")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+    testCompile(group = "org.mockito", name = "mockito-core", version = "2.23.0")
+    testCompile(group = "org.assertj", name = "assertj-core", version = "3.12.0")
+}
+
+tasks.jar {
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
